f9ccd8106f7f01055c1f8d3de84f74f58805c808
Alex Nascimento <alexjose68@hotmail.com>
2024-05-25 21:54:51 -0300

    feat: I created a new useState 'input' to work with an input that will receive the name of the task, properly using the useState parameters by placing the appropriate values ​​in the value and onChange properties to update the useState with each event added to the input, I also created a button and I sent an alert with a function to test its operation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
   const [tasks, setTasks] = useState([
     'Estudar React com TypeScript',
     'Comprar pão meio dia',
     'Estudadr inglês a noite'
   ]);
 
+  function handleRegister() {
+    alert("Botão está funcionando!")
+  }
+
   return (
     <div>
       <h1>Lista de Tarefas (Hello World)</h1>
+      <input
+        placeholder="Digite o título da tarefa..."
+        value={input}
+        onChange={ (e) => (setInput(e.target.value)) }
+      />
+
+      <button onClick={handleRegister}>Adicionar tarefas</button>
+      <h4>{input}</h4>
       <hr />
 
       <ul>{tasks.map((task, index) => (
