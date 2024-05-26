c596fd9df03f6522712aba9998e69ab4d3fb3a1f
Alex Nascimento <alexjose68@hotmail.com>
2024-05-25 21:42:14 -0300

    feat: I created a useState 'tasks' that returns an array, and I created the logic for the listing to render each item in the array with the map to traverse each item in the array and create elements each time the map traverses the useState array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    'Estudar React com TypeScript',
+    'Comprar pão meio dia',
+    'Estudadr inglês a noite'
+  ]);
 
   return (
     <div>
-      <h1>Lista de Tarefas (Hello World)</h1>        
+      <h1>Lista de Tarefas (Hello World)</h1>
+      <hr />
+
+      <ul>{tasks.map((task, index) => (
+        <li key={index}>{task}</li>
+      ))}</ul>
     </div>
   )
 }
