8a131ee02b5d3def27c573761753842490bfe758
Alex Nascimento <alexjose68@hotmail.com>
2024-05-26 13:11:59 -0300

    feat: I created the useState editTask and the handleEdit and handleSaveEdit functions to work on editing the useState tasks array and with the useState input string to edit the item/task from the index of the task we want to edit. And I created a conditional rendering for the button to describe whether to add or edit task.

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,59 @@ import { useState } from "react";
 
 function App() {
   const [input, setInput] = useState("");
-  const [tasks, setTasks] = useState([
-    'Estudar React com TypeScript',
-    'Comprar pão meio dia',
-    'Estudar inglês a noite'
-  ]);
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const [editTask, setEditTask] = useState({
+    enabled: false,
+    task: '',
+  });
 
   function handleRegister() {
     if(!input) {
       alert("Preencha o nome da sua tarefa!");
-      return; //
+      return; // Para evitar loop infinito
     }
 
+    if(editTask.enabled) { // se editTask == true vamos editar
+      handleSaveEdit();
+      return; // Para evitar loop infinito
+    }
+  
     // setTasks([...tasks, input]); /*(*)*/
     setTasks(ourTasks => [...ourTasks, input]);
     setInput('');
+  };
+
+  function handleSaveEdit() {
+    const findIndexTask = tasks.findIndex(task => task === editTask.task); // (***)
+    const allTasks = [...tasks]; // 'clono' meu array tasks para dentro da allTasks 
+
+    allTasks[findIndexTask] = input; // (****)
+    setTasks(allTasks);
+
+    // Após isso tenho que avisar que terminamos a edição:
+    setEditTask({
+      enabled: false,
+      task: '',
+    })
+
+    setInput(''); // Para limpar o campo
   }
 
   function handleDelete(index: number) {
-    const removeTask = tasks.filter( (_task, i)=> i !== index );
+    const removeTask = tasks.filter( (_task, i)=> i !== index ); // ('*')
+    console.log(index);
     console.log(removeTask);
     setTasks(removeTask);
   } // (**)
+
+  function handleEdit(item: string) {
+    setInput(item);
+    setEditTask({
+      enabled: true,
+      task: item,
+    })
+  }
 
   return (
     <div>
@@ -34,13 +65,16 @@ function App() {
         onChange={ (e) => (setInput(e.target.value)) }
       />
 
-      <button onClick={handleRegister}>Adicionar tarefas</button>
-      <h4>{input}</h4>
-      <hr />
+      <button onClick={handleRegister}>
+        {editTask.enabled ? "Atualizar tarefa": "Adicionar tarefa"}
+      </button>
+
+      <br /><br /><hr />
 
       {tasks.map((task, index) => (
         <ul key={index}>
           <li>{task}</li>
+          <button onClick={ () => handleEdit(task) } >Editar</button>
           <button onClick={ () => handleDelete(index) }>Excluir</button>
         </ul>
       ))}
@@ -63,6 +97,12 @@ export default App
 */
 
 /*
+  ('*') const removeTask = tasks.filter( (_task, i)=> i !== index );
+  // Para não passar o erro: 'task' is declared but its value is never read.ts(6133)
+  // passamos o prefixo task com um 'underscore', pois queremos usar o 2º parâmetro 'i'
+*/
+
+/*
   (**) Poderíamos construir a função handleDelete() pegando o valor do item/task assim:
 
   function handleDelete(item: string) {
@@ -75,4 +115,30 @@ export default App
   // repetisse por engano uma tarefa teríamos um erro por ter duas keys iguais, tendo um caso de key não única,
   // por isso escolhemos usar o index ao invés de task para a key assim como decidimos usar index para pegar
   // a posição do item que queremos deletar na função handleDelete!
+*/
+
+/* (***)
+  // const findIndexTask = tasks.findIndex(task => task === editTask.task);
+  // o findIndex() percorre nossa lista e procura (comparando a condição) a posição do item/task que queremos editar
+  // e nos retorna a posição!
+*/
+
+/* (****)
+  // tendo a posição na const findIndexTask
+  // e busco a posição do item/task que quero editar dentro do array, e que vai ser igual ao que digito no input:
+    allTasks[findIndexTask] = input;
+
+    // e agora altero o array com o usestate setTasks:
+    setTasks(allTasks);
+*/
+
+/*
+  <button onClick={handleRegister}>
+    {editTask.enabled ? "Atualizar tarefa": "Adicionar tarefa"}
+  </button>
+
+  // Renderização condicional:
+  // Se editTask tiver enabled: true
+  // o texto do button será "Atualizar tarefa"
+  // se não estiver true será "Adicionar tarefa"
 */
