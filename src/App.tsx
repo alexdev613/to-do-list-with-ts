2f02ac2f2d9dd5b0959878b152bc59c987bb459d
Alex Nascimento <alexjose68@hotmail.com>
2024-05-25 22:36:00 -0300

    feat: With the handleRegister function I implemented that if there is nothing in the state 'input', an alert will appear asking to fill in a task in the field and I also put a return in the scope of this if so that an infinite loop does not occur. And within the same function I asked for whatever is inside the useState 'input' to be added to the useState 'tasks' list (if there is something inside the input)

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,14 @@ function App() {
   ]);
 
   function handleRegister() {
-    alert("Botão está funcionando!")
+    if(!input) {
+      alert("Preencha o nome da sua tarefa!");
+      return; //
+    }
+
+    // setTasks([...tasks, input]); /*(*)*/
+    setTasks(ourTasks => [...ourTasks, input]);
+    setInput('');
   }
 
   return (
@@ -33,3 +40,15 @@ function App() {
 }
 
 export default App
+
+/*  setTasks([...tasks, input]);
+  (*) já funcionaria assim com spread operator para o nosso useState tasks para
+  apegar tudo e acrescentar o que tem no meu useState input, mas vamos fazer de outro jeito p/aumentar o repertório 
+
+    Com uma função anônima chamo agora meu estado tasks como ourTasks, e uso um spread operator para pegar
+  tudo dentro do meu useState e acrescento o que tem no input:
+
+    setTasks(ourTasks => [...ourTasks, input]);
+  
+  É só um jeito diferente de fazer setTasks([...tasks, input]);
+*/
