e98058b98f485ce4b91c5c8a50770b1c4cff65f6
Alex Nascimento <alexjose68@hotmail.com>
2024-05-27 23:46:29 -0300

    feat: I implemented the logic to save my useState tasks list so that when we add, change an item from the list or delete an item from the array to localStorage, and we use a useEffect so that when the application component is mounted it searches localStorage for the key we saved our data. Making the appropriate conversions when necessary.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [input, setInput] = useState("");
@@ -8,6 +8,15 @@ function App() {
     enabled: false,
     task: '',
   });
+
+  useEffect(() => {
+    const tarefasSalvas = localStorage.getItem("@todolist-ts");
+
+    if(tarefasSalvas) {
+      setTasks(JSON.parse(tarefasSalvas));
+    }
+
+  }, []); // (x¹) // (x⁵)
 
   function handleRegister() {
     if(!input) {
@@ -23,6 +32,9 @@ function App() {
     // setTasks([...tasks, input]); /*(*)*/
     setTasks(ourTasks => [...ourTasks, input]);
     setInput('');
+
+    localStorage.setItem("@todolist-ts", JSON.stringify([...tasks, input])); // (x²)
+  
   };
 
   function handleSaveEdit() {
@@ -39,6 +51,9 @@ function App() {
     })
 
     setInput(''); // Para limpar o campo
+
+    localStorage.setItem('@todolist-ts', JSON.stringify(allTasks)); // (x³)
+
   }
 
   function handleDelete(index: number) {
@@ -46,6 +61,9 @@ function App() {
     console.log(index);
     console.log(removeTask);
     setTasks(removeTask);
+
+    localStorage.setItem("@todolist-ts", JSON.stringify(removeTask)); // (x⁴)
+
   } // (**)
 
   function handleEdit(item: string) {
@@ -141,4 +159,85 @@ export default App
   // Se editTask tiver enabled: true
   // o texto do button será "Atualizar tarefa"
   // se não estiver true será "Adicionar tarefa"
+*/
+
+/*
+  (x¹)
+    // Quando criamos um useEffect e passamos comandos na função anônima mas deixamos array de dependências vazio,
+    // então este useEffect só executará a função anônima uma única vez que é quando o componente for montado!
+    // Agora se colocarmos alguma depêndência no array de dependências, além da função anônima ser executada quando
+    // o componente for montado ela também será executada quando o valor do que passarmos no array de dependências mudar!
+*/
+
+/*
+    // (x²)
+
+    localStorage.setItem("@todolist-ts", JSON.stringify([...tasks, input]));
+
+    // Primeiro é importatnte saber que o localStorage é um objeto, e o setItem é um método deste objeto.
+    // Esse método é usado para adicionar um item ao armazenamento local do navegador (localStorage) com
+    // uma chave específica e um valor. O método setItem aceita dois parâmetros: a chave (uma string) e o
+    // valor (também uma string).
+    // "@todolist-ts" é a chave. E para o valor pegamos todas as tasks já lançadas com o spread operation
+    // e acrescentamos o input em um array, mas como o valor deve ser uma string, usamos o JSON.stringfy()
+    // para converter o array em uma string. Podemos observar as mudanças das adições em inspecionar e acessar
+    // a aba Application/Aplicativo, indo em Armazenamento Local podemos encontar nossa chave @todolist-ts
+*/
+
+/*
+    // (x³)
+
+    localStorage.setItem('@todolist-ts', JSON.stringify(allTasks))
+
+    // Aqui precisamos fazer a mesma coisa, pois neste caso de uso temos a função de alterar um item
+    // do array. Então chamamos o objeto localStorage para salvar no nosso armazenamento local com o
+    // método setItem colocamos no primeiro parâmetro a mesma chave '@todolist-ts', pois vamos salvar
+    // as alterações também nela. e aqui usamos a const alltasks para receber nosso array com o item
+    // alterado segundo a lógica! Como é um array, nós usamos o JSON.stringfy para converter o valor
+    // para string, como pede o método setItem
+*/
+
+/*
+    // (x⁴)
+
+    localStorage.setItem("@todolist-ts", JSON.stringify(removeTask));
+
+    // Para este caso de uso, que é o caso de deletar um item do nosso array. Ao deletar um item
+    // para salvar no armazenamento locao do navegador preciso chamar novamento o objeto localStorage
+    // e seu método setItem, passando como primeiro parâmetro a nossa chave "@todolist-ts", e agora a
+    // nossa const removeTask, que é o array do useState tasks atualizado com o item que deletamos,
+    // em resumo é a variável que passamos para o setTasks, e sendo um array precisamo converter para
+    // string como fizemos anteriomente, com o JSON.stringfy.
+*/
+
+/*
+  // (x⁵)
+  
+  useEffect(() => {
+    const tarefasSalvas = localStorage.getItem("@todolist-ts");
+
+    if(tarefasSalvas) {
+      setTasks(JSON.parse(tarefasSalvas));
+    }
+
+  }, []);
+  
+  // Para quadno o componente for montado precisamos pegar o que temos no nosso aramzenamento local
+  // fazemos isso na linha: const tarefasSalvas = localStorage.getItem("@todolist-ts");
+  // O método getItem busca no localStorage a nossa chave "@todolist-ts", então já temos acesso ao
+  // que tiver salvo na nossa chave. Então, já que o que tem dentro do nosso localStorage é uma string,
+  // precisamos converter para um array, já que nosso useState tasks é quem recebe esses itens para renderizar
+  // em tela nossas tarefas! Então criamos o if:
+
+    if(tarefasSalvas) {
+      setTasks(JSON.parse(tarefasSalvas));
+    }
+
+  // SE TIVER ALGO na nossa const tarefasSalvas, preencha o setTasks (que é nosso parâmetro de mudança do nosso
+  // useState tasks), conversetendo para JSON, ou seja, para uma coleção array, a string recebida pela const
+  // tarefasSalvas, que é o que obtemos no nosso localStorage na chave "todolist-ts".
+
+  // Com isto, quando o nosso componente for montado, se houver algo na nossa chave "todolist-ts" salvo, teremos
+  // acesso na nossa aplicação seguindo a lógica que construimos até agora!
+
 */
